b0d0526781956a1a759ceb7e29fe7d8ace6ea646
Joe Johnston <jjoh1051@ford.com>
2018-07-29 11:22:41 -0400

    added classes for a level and its backgrounds

---
+++ lib/slick_ruby_game/level.rb
@@ -0,0 +1,6 @@
+require_relative '../game_object'
+
+class Level < SlickRubyGame::GameObject
+    attr_accessor :layers
+
+end
---
+++ spec/slick_ruby_game/background_spec.rb
@@ -0,0 +1,6 @@
+include Java
+require_relative '../../../lib/slick_ruby_game/include_slick'
+require_relative '../../../lib/slick_ruby_game/background'
+
+describe Background do
+end
