83b6ee91086ca28ada9170394799124fdfe9e78e
Joe Johnston <jjoh1051@ford.com>
2018-07-30 14:24:12 -0400

    got the example to render a sprite, added a spritesheet image to the example resources folder

--- example/example_game.rb
+++ example/example_game.rb
@@ -1,5 +1,6 @@
 require_relative '../lib/slick_ruby_game'
 require_relative '../lib/slick_ruby_game/picture'
+require_relative '../lib/slick_ruby_game/sprites/sprite'
 
 game = SlickRubyGame::MainGameLoop.new('A Day at the Station')
 
@@ -8,7 +9,18 @@ background.image_location = './res/GFV_train_HD.jpg'
 background.start_x = 0
 background.start_y = 0
 
+bounding_llama = SlickRubyGame::Sprite.new
+bounding_llama.spritesheet_location = './res/llama.png'
+bounding_llama.sprite_width = 48
+bounding_llama.sprite_height = 48
+bounding_llama.render_width = 128
+bounding_llama.render_height = 128
+bounding_llama.x_offset = 1200
+bounding_llama.y_offset = 900
+bounding_llama.animation_speed = 100
+
 game.add_game_object(background)
+game.add_game_object(bounding_llama)
 
 game_container = AppGameContainer.new(game)
 game_width = 1920
