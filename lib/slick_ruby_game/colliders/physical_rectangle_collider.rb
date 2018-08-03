a0858b776d9788746623da10387fff265abe768a
Joe Johnston <jjoh1051@ford.com>
2018-08-03 08:05:52 -0400

    started on a physical rectangle collider

---
+++ lib/slick_ruby_game/colliders/physical_rectangle_collider.rb
@@ -0,0 +1,11 @@
+require_relative 'abstract_physical_collider'
+
+module SlickRubyGame::Colliders
+    class PhysicalRectangleCollider < AbstractPhysicalCollider
+
+        def initialize
+            super
+        end
+        
+    end
+end
---
+++ spec/slick_ruby_game/colliders/physical_rectangle_collider_spec.rb
@@ -0,0 +1,5 @@
+require_relative '../../../lib/slick_ruby_game/colliders/physical_rectangle_collider'
+
+describe 'physical rectangle collider' do
+end
+
