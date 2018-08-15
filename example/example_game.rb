33edc932023ad086930c5f5e1619edf775f6d981
Joe Johnston <jjoh1051@ford.com>
2018-08-15 07:57:01 -0400

    got the gem working

--- example/example_game.rb
+++ example/example_game.rb
@@ -11,49 +11,13 @@ require_relative 'example_input'
 #     end
 # end
 
-class SpriteMovementHandler < SlickRubyGame::GameObject
-
-    def update(gc, delta)
-        super
-        input = gc.get_input()
-        move_distance = delta * parent.movement_speed
-
-        is_moving_right = input.is_key_down(Input::KEY_RIGHT)
-        is_moving_left = input.is_key_down(Input::KEY_LEFT)
-        is_moving_up = input.is_key_down(Input::KEY_UP)
-        is_moving_down = input.is_key_down(Input::KEY_DOWN)
-        is_moving = (is_moving_right || is_moving_left || is_moving_up || is_moving_down)
-
-        if is_moving
-            parent.start_moving
-        else
-            parent.stop_moving
-            parent.reset_animation
-        end
-
-        if is_moving_right
-            parent.offset_x = parent.offset_x + move_distance
-        end
-        if is_moving_left
-            parent.offset_x = parent.offset_x - move_distance
-        end
-
-        if is_moving_up
-            parent.offset_y = parent.offset_y - move_distance
-        end
-        if is_moving_down
-            parent.offset_y = parent.offset_y + move_distance
-        end
-    end
-end
-
 game = SlickRubyGame::MainGameLoop.new('A Day at the Station')
 level = SlickRubyGame::Level.new
 level.width = 1920
 level.height = 1080 #these should be handled at a higher place than the level
 camera_perspective = SlickRubyGame::AreaCameraPerspective.new
 camera_perspective.top_percentage_at(0.1, 600)
-camera_perspective.bottom_percentage_at(3, 1080)
+camera_perspective.bottom_percentage_at(2, 1080)
 
 game.add_game_object(level)
 
