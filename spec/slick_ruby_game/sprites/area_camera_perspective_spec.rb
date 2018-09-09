6ca2f3c8f16b4555752f1082f2a894d9d1720435
Joe Johnston <jjoh1051@ford.com>
2018-09-09 09:00:07 -0400

    added some tests for the area camera perspective

--- spec/slick_ruby_game/sprites/area_camera_perspective_spec.rb
+++ spec/slick_ruby_game/sprites/area_camera_perspective_spec.rb
@@ -35,6 +35,28 @@ describe 'area camera perspective' do
     describe '#scale_value_for' do
         it 'should subtract the top offset from the offset passed in' do
             expect(@perspective.scale_value_for(50)).to match 0.5
+            expect(@perspective.scale_value_for(75)).to match 1.0
+            expect(@perspective.scale_value_for(100)).to match 1.5
+            expect(@perspective.scale_value_for(150)).to match 2.5
+        end
+
+        it 'should work when the offset is above (lower than) the top value' do
+            expect(@perspective.scale_value_for(49)).to match 0.48
+            expect(@perspective.scale_value_for(39)).to match 0.28
+            expect(@perspective.scale_value_for(35)).to match 0.2
+            expect(@perspective.scale_value_for(32)).to match 0.14
+            expect(@perspective.scale_value_for(30)).to match 0.09999999999999998
+            expect(@perspective.scale_value_for(29)).to match 0.08000000000000002
+            expect(@perspective.scale_value_for(25)).to match 0.0 # << this is essentially the "horizon" in the scene
+        end
+
+        it 'should work when the offset is below (higher than) the bottom value' do
+            expect(@perspective.scale_value_for(151)).to match 2.52
+            expect(@perspective.scale_value_for(160)).to match 2.7
+            expect(@perspective.scale_value_for(170)).to match 2.9
+            expect(@perspective.scale_value_for(175)).to match 3.0
+            expect(@perspective.scale_value_for(180)).to match 3.1
+            expect(@perspective.scale_value_for(200)).to match 3.5
         end
     end
 
