338cb63ee752855fbe31205856a8864e306e6566
Joe Johnston <jjoh1051@ford.com>
2018-09-12 22:54:34 -0400

    made an actionlistener that uses a block

--- lib/ide/commands/add_level_command.rb
+++ lib/ide/commands/add_level_command.rb
@@ -15,11 +15,24 @@ module SlickRubyGame
             class AddLevelOkActionListener 
                 include ActionListener
 
-                
-
-
                 def action_performed(event)
                     puts 'ok clicked'
+                end
+            end
+
+            class BlockActionListener
+                include ActionListener
+
+                def self.on_action(&block)
+                    return self.new(block)
+                end
+
+                def initialize(block)
+                    @block = block
+                end
+
+                def action_performed(event)
+                    @block.call
                 end
             end
 
@@ -28,7 +41,11 @@ module SlickRubyGame
 
                 def execute(ide_state)
                     dialog = AddLevelDialog.new
-                    dialog.set_ok_button_listener(AddLevelOkActionListener.new)
+                    listener = BlockActionListener.on_action do
+                        model = dialog.get_model
+                        puts 'called: ' + model.get_name + ' ' + model.get_default_layer_name
+                    end
+                    dialog.set_ok_button_listener(listener)
                     dialog.set_visible(true)
                 end
 
@@ -38,4 +55,3 @@ module SlickRubyGame
 end
 
 SlickRubyGame::IDE::Commands::AddLevelCommand.new.execute(nil)
-
